1a3f2cdb83caec42b5f64532285e37ab156f33da
Daniel Czarnocki <danielczarnocki@gmail.com>
2023-01-05 13:24:31 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("console check");
+
+let button = document.querySelector(".button");
+let foto = document.querySelector(".js-foto");
+let showHide = document.querySelector(".js-showHide");
+
+button.addEventListener("click", () => {
+    foto.classList.toggle("foto__noborder");
+
+    showHide.innerText = foto.classList.contains("foto__noborder") ? "Pokaż" : "Ukryj";
+    });
+
+
+    
