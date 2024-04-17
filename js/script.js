fba6d092fa52fc8fd7c40f198f6d2f9f59b8720f
Daniel Czarnocki <danielczarnocki@gmail.com>
2024-04-17 19:33:34 +0200

    added functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
-console.log("console check");
-
-let button = document.querySelector(".js-button");
-let foto = document.querySelector(".js-foto");
-let showHide = document.querySelector(".js-showHide");
-
-button.addEventListener("click", () => {
-    foto.classList.toggle("foto__noborder");
-
-    showHide.innerText = foto.classList.contains("foto__noborder") ? "Pokaż" : "Ukryj";
-    });
-
-
+{
+    const welcome = () => {
+        console.log("console check");
+    }
+    /*
+    const onChangeBorderClick = () => {
+        foto.classList.toggle("foto__noborder");
+        showHide.innerText = foto.classList.contains("foto__noborder") ? "Pokaż" : "Ukryj";
+    };
     
+    welcome();
+    
+    let button = document.querySelector(".js-button");
+    let foto = document.querySelector(".js-foto");
+    let showHide = document.querySelector(".js-showHide");
+    
+    button.addEventListener("click", onChangeBorderClick);
+    */
+
+    const onChangeBackgroundClick = () => {
+        const background = document.querySelector(".js-background");
+        const backgroundShowHide = document.querySelector(".js-backgroundShowHide");
+        background.classList.toggle("background__noFoto");
+        backgroundShowHide.innerText = background.classList.contains("background__noFoto") ? "Pokaż" : "Ukryj";
+    };
+
+    welcome();
+
+    const init = () => {
+    const backgroundButton = document.querySelector(".js-backgroundButton");
+    backgroundButton.addEventListener("click", onChangeBackgroundClick);
+    }
+
+    init();
+    
+}
+
+
