00fd25c76bf861f018c69a09a1c95bba140579a2
Daniel Czarnocki <danielczarnocki@gmail.com>
2024-04-21 19:40:02 +0200

    removed unused script and comments

--- js/script.js
+++ js/script.js
@@ -2,21 +2,7 @@
     const welcome = () => {
         console.log("console check");
     }
-    /*
-    const onChangeBorderClick = () => {
-        foto.classList.toggle("foto__noborder");
-        showHide.innerText = foto.classList.contains("foto__noborder") ? "Pokaż" : "Ukryj";
-    };
     
-    welcome();
-    
-    let button = document.querySelector(".js-button");
-    let foto = document.querySelector(".js-foto");
-    let showHide = document.querySelector(".js-showHide");
-    
-    button.addEventListener("click", onChangeBorderClick);
-    */
-
     const onChangeBackgroundClick = () => {
         const background = document.querySelector(".js-background");
         const backgroundShowHide = document.querySelector(".js-backgroundShowHide");
